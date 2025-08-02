a72a42e94d92c7c1ea83fc33988a97a2d8532f17
Cereny <56936296+cerenyilmazjinx@users.noreply.github.com>
2025-08-02 20:44:28 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/nextdisk/main.go
@@ -0,0 +1,23 @@
+// cmd/nextdisk/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"nextdisk/internal/nextdisk"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := nextdisk.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
